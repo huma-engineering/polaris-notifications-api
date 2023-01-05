706e2e8355d9e0d4dd0fafd8dd103a1b300a56ab
VladStorozhenko <vladyslav.storozhenko@huma.com>
2023-01-05 17:07:00 +0200

    Add github actions workflow

--- .circleci/run_integration_tests.sh
+++ .circleci/run_integration_tests.sh
@@ -3,20 +3,20 @@
 set -eux
 
 # The Dockerfiles require these
-touch build-circleci.txt
-touch build-githash.txt
+#touch build-circleci.txt
+#touch build-githash.txt
 
 TEST_CONTAINER_NAME=$(git remote -v | head -1 | cut -d/ -f2 | sed 's/-api.*/-integration-tests/' | sed 's/polaris/dhos/')
 
 cd integration-tests
 
 # Enable ReportPortal integration if on the default branch
-if [ $CIRCLE_BRANCH == $DEFAULT_BRANCH ]; then
-  echo "Enabling reportportal integration"
-  export BEHAVE_ARGS="-D rp_enable=True -D step_based=True"
-  export ENVIRONMENT=dev
-  export RELEASE=$(git describe --tags | sed s/v//g)
-fi
+#if [ $CIRCLE_BRANCH == $DEFAULT_BRANCH ]; then
+#  echo "Enabling reportportal integration"
+#  export BEHAVE_ARGS="-D rp_enable=True -D step_based=True"
+#  export ENVIRONMENT=dev
+#  export RELEASE=$(git describe --tags | sed s/v//g)
+#fi
 
 # Start the containers, backgrounded so we can do docker wait
 # Pre pulling the postgres image so wait-for-it doesn't time out
@@ -25,14 +25,14 @@ docker-compose pull
 docker-compose up --build --force-recreate -d
 
 # Wait for the integration-tests container to finish, and assign to RESULT
-RESULT=$(docker wait ${TEST_CONTAINER_NAME})
+RESULT=$(docker wait "${TEST_CONTAINER_NAME}")
 
 # Print logs based on the test results
 if [ "$RESULT" -ne 0 ];
 then
   docker-compose logs
 else
-  docker-compose logs ${TEST_CONTAINER_NAME}
+  docker-compose logs "${TEST_CONTAINER_NAME}"
 fi
 
 # Stop the containers
